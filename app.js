dd9c9bc391c6b7413cb181777f84b2ba50bacd5f
Mariah <mariahschock@gmail.com>
2022-04-28 13:04:58 -0700

    trying to get images to appear

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 // import functions and grab DOM elements
+const locationSelect = document.getElementById('location-section');
+const architectureSelect = document.getElementById('architecture-section');
 
+const locationEl = document.getElementById('location');
+const architectureEl = document.getElementById('architecture');
 // let state
 
 // set event listeners 
+locationSelect.addEventListener('change', () => {
+    locationEl.style.backgroundImage = `url('../assets/${locationSelect.value}.png')`;
+});
+
+architectureSelect.addEventListener('change', () => {
+    architectureEl.style.backgroundImage = `url('../assets/${architectureSelect.value}.png')`;
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
