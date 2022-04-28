3f8960b9f577c10ca83fdfbb17a554f9cf4bd07d
Mariah <mariahschock@gmail.com>
2022-04-28 16:05:08 -0700

    slogan button

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 // import functions and grab DOM elements
 const locationSelect = document.getElementById('location-select');
 const architectureSelect = document.getElementById('architecture-select');
-
 const locationDiv = document.getElementById('location');
 const architectureDiv = document.getElementById('architecture');
+const sloganButton = document.getElementById('slogan-button');
+const sloganInput = document.getElementById('slogan-input');
+const reportEl = document.getElementById('report');
 // let state
+let locationCount = 0;
+let architectureCount = 0;
+
+let slogan = [];
 
 // set event listeners 
 locationSelect.addEventListener('change', () => {
+    locationCount++;
     locationDiv.style.backgroundImage = `url('./assets/${locationSelect.value}.png')`;
 });
 
 architectureSelect.addEventListener('change', () => {
+    architectureCount++;
     architectureDiv.style.backgroundImage = `url('./assets/${architectureSelect.value}.png')`;
+});
+
+sloganButton.addEventListener('click', () => {
+    slogan.push(sloganInput.value);
+    sloganInput.value = ' ';
+    displaySlogans();
 });
   // get user input
   // use user input to update state 
