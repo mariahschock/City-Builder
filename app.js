7b781cc22e6d0926ae0d20988a481f67dfad0507
Mariah <mariahschock@gmail.com>
2022-04-28 17:04:01 -0700

    displaying slogans

--- app.js
+++ app.js
@@ -6,28 +6,46 @@ const architectureDiv = document.getElementById('architecture');
 const sloganButton = document.getElementById('slogan-button');
 const sloganInput = document.getElementById('slogan-input');
 const reportEl = document.getElementById('report');
+const sloganEl = document.getElementById('slogan');
 // let state
 let locationCount = 0;
 let architectureCount = 0;
 
-let slogan = [];
+let slogans = [];
 
 // set event listeners 
 locationSelect.addEventListener('change', () => {
     locationCount++;
     locationDiv.style.backgroundImage = `url('./assets/${locationSelect.value}.png')`;
+    displayStats();
 });
 
 architectureSelect.addEventListener('change', () => {
     architectureCount++;
     architectureDiv.style.backgroundImage = `url('./assets/${architectureSelect.value}.png')`;
+    displayStats();
 });
 
 sloganButton.addEventListener('click', () => {
-    slogan.push(sloganInput.value);
+    slogans.push(sloganInput.value);
     sloganInput.value = ' ';
     displaySlogans();
 });
+
+function displayStats() {
+    reportEl.textContent = `You have changed the location ${locationCount} times and the architecture ${architectureCount} times.`;
+}
+
+function displaySlogans() {
+    sloganEl.textContent = '';
+    const slogans = document.getElementById('slogans');
+
+    for (let tagline of slogans) {
+        const li = document.createElement('li');
+        li.textContent = tagline;
+        slogans.append(li);
+    }
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
